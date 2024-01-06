ff12cd1da3f599fa401e2a21ab740cbf899b6202
webdesirewebsolution <webdeveloperji71@gmail.com>
2024-01-06 12:03:08 +0530

    update socket

--- frontend/src/context/SocketProvider.jsx
+++ frontend/src/context/SocketProvider.jsx
@@ -10,7 +10,7 @@ export const useSocket = () => {
 
 export const SocketProvider = (props) => {
 
-    const socket = useMemo(() => io("localhost:8000"), []);
+    const socket = useMemo(() => io("https://react-videocall-ygwc.onrender.com"), []);
 
     return (
         <SocketContext.Provider value={socket}>
